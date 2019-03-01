a0f414cae95fdd105cfcb4ba14b701c3336c634c
Evana Islam <>
2019-03-02 02:38:47 +1100

    [create_view_model] - Add CellViewModel and process contacts data

--- Contacts/Module/Contacts/ViewModel/ContactListViewModel.swift
+++ Contacts/Module/Contacts/ViewModel/ContactListViewModel.swift
@@ -24,9 +24,16 @@ class ContactListViewModel {
         }
     }
     
+    private var cellViewModels: [ContactCellViewModel] = [ContactCellViewModel]() {
+        didSet {
+            self.reloadCollectionViewClosure?()
+        }
+    }
+    
     var showAlertClosure: (()->())?
     var updateLoadingStatus: (()->())?
-    
+    var reloadCollectionViewClosure: (()->())?
+
     init( contactService: ContactServiceProtocol = ContactService()) {
         self.contactService = contactService
     }
@@ -44,7 +51,29 @@ class ContactListViewModel {
                     self.alertMessage = "Something went wrong"
                     return
                 }
+                self.processContacts(contacts: contacts)
             }
         }
     }
+    
+    private func processContacts(contacts: [Contact]) {
+        self.contacts = contacts
+        var viewModels = [ContactCellViewModel]()
+        for contact in contacts {
+            viewModels.append(createCellViewModel(contact: contact))
+        }
+        cellViewModels = viewModels
+    }
+    
+    private func createCellViewModel( contact: Contact ) -> ContactCellViewModel {
+        #warning("Need to put Avatar image")
+        return ContactCellViewModel(avatarImage: contact.gender.rawValue, name: "\(contact.firstName) \(contact.lastName)", email: contact.email, isFavorite: contact.isFavorite)
+    }
+}
+
+struct ContactCellViewModel {
+    let avatarImage: String
+    let name: String
+    let email: String
+    var isFavorite: Bool
 }
