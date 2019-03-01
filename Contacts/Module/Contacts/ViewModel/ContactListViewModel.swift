3fe2206fe70895e2cbd0101ee712c1918e290757
Evana Islam <>
2019-03-02 02:34:55 +1100

    [create_view_model] - Add contact fetch functionality

--- Contacts/Module/Contacts/ViewModel/ContactListViewModel.swift
+++ Contacts/Module/Contacts/ViewModel/ContactListViewModel.swift
@@ -7,3 +7,44 @@
 //
 
 import Foundation
+
+class ContactListViewModel {
+    let contactService: ContactServiceProtocol
+    var contacts: [Contact]?
+    
+    var isLoading: Bool = false {
+        didSet {
+            self.updateLoadingStatus?()
+        }
+    }
+    
+    var alertMessage: String? {
+        didSet {
+            self.showAlertClosure?()
+        }
+    }
+    
+    var showAlertClosure: (()->())?
+    var updateLoadingStatus: (()->())?
+    
+    init( contactService: ContactServiceProtocol = ContactService()) {
+        self.contactService = contactService
+    }
+    
+    
+    func fetchContact() {
+        self.isLoading = true
+        contactService.fetchContacts { [weak self] success, contacts, error in
+            guard let self = self else { return }
+            self.isLoading = false
+            if let error = error {
+                self.alertMessage = error.localizedDescription
+            } else {
+                guard let contacts = contacts else {
+                    self.alertMessage = "Something went wrong"
+                    return
+                }
+            }
+        }
+    }
+}
