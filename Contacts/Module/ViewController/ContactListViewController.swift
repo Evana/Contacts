da5103a2cb7c16a8f6b82c422e5616c68b225845
Evana Islam <>
2019-03-02 03:18:36 +1100

    [create_view_controller] - Initialise viewModel

--- Contacts/Module/ViewController/ContactListViewController.swift
+++ Contacts/Module/ViewController/ContactListViewController.swift
@@ -13,8 +13,49 @@ class ContactListViewController: CollectionViewController {
     var showMoreButton: UIButton!
     var activityIndicator: UIActivityIndicatorView!
     
+    var contactListViewModel: ContactListViewModel = {
+        return ContactListViewModel()
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    func initialiseViewModel() {
         
+        // Naive binding
+        contactListViewModel.showAlertClosure = { [weak self]  in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                if let message = self.contactListViewModel.alertMessage {
+                    self.showAlert( message )
+                }
+            }
+        }
+        
+        contactListViewModel.updateLoadingStatus = { [weak self]  in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                if self.contactListViewModel.isLoading {
+                    self.activityIndicator.startAnimating()
+                } else {
+                    self.activityIndicator.stopAnimating()
+                }
+            }
+        }
+        
+        contactListViewModel.reloadCollectionViewClosure = { [weak self]  in
+            guard let self = self else { return }
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+        contactListViewModel.fetchContact()
+    }
+    
+    private func showAlert( _ message: String ) {
+        let alert = UIAlertController(title: "Alert", message: message, preferredStyle: .alert)
+        alert.addAction( UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
