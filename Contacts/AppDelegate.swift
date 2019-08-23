7899ae0db75958c216349dd364b457fb62bbfbd2
Evana Islam <>
2019-08-23 16:06:06 +1000

    Commit to check bitrise setup working

--- Contacts/AppDelegate.swift
+++ Contacts/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         self.window = UIWindow(frame: UIScreen.main.bounds)
         let navController = UINavigationController()
         navController.navigationBar.barTintColor = .white
